a424092de48086bb9e22f0454b80de5fa2ad526a
root <ian.brennan@icloud.com>
2015-06-18 14:15:00 +0000

    extra punctuation

--- web/server/server.js
+++ web/server/server.js
@@ -13,7 +13,7 @@ var configServer = require('./lib/config/server');
 
 // app parameters
 var app = express();
-var routes = require('/routes/index');
+var routes = require('./routes/index');
 app.set('port', configServer.httpPort);
 app.use(express.static(configServer.staticFolder));
 app.use(morgan('dev'));
