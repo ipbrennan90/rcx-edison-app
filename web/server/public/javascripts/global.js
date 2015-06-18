0e75c1958224b958b84b20f71f86c4b5443eaa70
Ian Brennan <ian.brennan@icloud.com>
2015-06-18 11:16:54 -0600

    jquery additions

--- web/server/public/javascripts/global.js
+++ web/server/public/javascripts/global.js
@@ -1,17 +1,21 @@
 var decoder = {'keyDownDecoder' : {'87':'w', '65':'a', '83':'s', '68':'d', '69':'e', '81':'q'}, 'keyUpDecoder': {'65':'r', '68':'r'}}
-var wsUrl = 'ws://edison.local:8084/';
+
 
 // Show loading notice
-var canvas = document.getElementById('canvas-video');
-// var ctx = canvas.getContext('2d');
-// ctx.fillStyle = '#333';
-// ctx.fillText('Loading...', canvas.width/2-30, canvas.height/3);
+
+
 
 // Start the player
-var client = new WebSocket(wsUrl);
-var player = new jsmpeg(client, { canvas:canvas });
+
 
 $(document).ready(function(){
+  var wsUrl = 'ws://edison.local:8084/';
+  var canvas = $('#canvas-video');
+  var ctx = canvas.getContext('2d');
+  ctx.fillStyle = '#333';
+  ctx.fillText('Loading...', canvas.width/2-30, canvas.height/3);
+  var client = new WebSocket(wsUrl);
+  var player = new jsmpeg(client, { canvas:canvas });
   function wait(keyCode, decodeObject){
 
     console.log('post fired')
