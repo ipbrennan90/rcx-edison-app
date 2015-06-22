fe06246b2b4d54c60d6ff553759362dde4a6d393
Ian Brennan <ian.brennan@icloud.com>
2015-06-21 19:17:54 -0600

    suckycode steering actually working

--- web/server/public/javascripts/global.js
+++ web/server/public/javascripts/global.js
@@ -7,7 +7,10 @@
 
 
 $(document).ready(function(){
-  var decoder = {'keyDownDecoder' : {'87':'w', '65':'a', '83':'s', '68':'d', '69':'e', '81':'q'}, 'keyUpDecoder': {'65':'r', '68':'r'}}
+  var center = 90
+  var angleR = 90
+  var angleL = 90
+  var decoder = {'keyDownDecoder' : {'87':'w', '65':center.toString(), '83':'s', '68':center.toString(), '69':center.toString(), '81':'q'}, 'keyUpDecoder': {'65':'r', '68':'r'}}
   var wsUrl = 'ws://edison.local:8084/';
   var canvas = document.getElementById('canvas-video');
   var ctx = canvas.getContext('2d');
@@ -21,20 +24,46 @@ $(document).ready(function(){
     $.ajax({
       type: "POST",
       url: "/",
-      data: {"message": decoder[decodeObject][keyCode.toString()]},
+      data: {"message": decoder[decodeObject][keyCode.toString()] + "\n"},
       success: function(data){
         console.log(data.toString())
       }
     });
 
   }
+  var decrement = 0
+  var increment = 0
 
   $('body').on("keydown", function keyDown(e){
 
     var keyCode = e.keyCode;
-    console.log(keyCode)
+
     if(decoder['keyDownDecoder'].hasOwnProperty(keyCode)){
-      setTimeout(function(){wait(keyCode, 'keyDownDecoder');}, 10)
+      if(keyCode == 65 || keyCode == 68){
+        if(keyCode ==65){
+          if(angleL > 45){
+            angleL -= 5;
+          }
+          decoder['keyDownDecoder'][keyCode.toString()] = (angleL).toString();
+          setTimeout(function(){wait(keyCode, 'keyDownDecoder');}, 10);
+        }else{
+          if(angleR < 135){
+            angleR += 5;
+          }
+          decoder['keyDownDecoder'][keyCode.toString()] = (angleR).toString();
+          setTimeout(function(){wait(keyCode, 'keyDownDecoder');}, 10);
+        }
+
+      }else{
+        angleR = 90;
+        angleL = 90;
+        
+        decoder['keyDownDecoder']['65'] = center.toString();
+        decoder['keyDownDecoder']['68'] = center.toString();
+        setTimeout(function(){wait(68, 'keyDownDecoder');}, 800);
+        setTimeout(function(){wait(keyCode, 'keyDownDecoder');}, 100);
+      }
+
     }
 
   });
